fbc05a86b407fb28f64990be4c19740c6607b201
coumbafallydiokhane@gmail.com <maman@gamil.com>
2025-07-23 17:38:38 +0000

    Sauvegarde des modifications avant changement de branche

--- isi-school-frontend/src/app/app.config.ts
+++ isi-school-frontend/src/app/app.config.ts
@@ -1,9 +1,10 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig} from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 
 // On importe ce dont on a besoin
-import { provideHttpClient, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { provideHttpClient} from '@angular/common/http';
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
 import { JwtInterceptor } from './core/interceptors/jwt.interceptor';
 
 
