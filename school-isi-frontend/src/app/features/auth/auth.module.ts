46a648fe1aabf6ad1489b50d7cf7ba6768709933
coumbafallydiokhane@gmail.com <maman@gamil.com>
2025-07-23 23:04:18 +0000

    Enregistrement des modifications avant de changer de branche

--- school-isi-frontend/src/app/features/auth/auth.module.ts
+++ school-isi-frontend/src/app/features/auth/auth.module.ts
@@ -1,12 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-import { LoginComponent } from './pages/login/login.component'; // Importer le composant
 import { ReactiveFormsModule } from '@angular/forms'; // Importer le module de formulaires
 
 @NgModule({
   declarations: [
-    LoginComponent /
+    
   ],
   imports: [
     CommonModule,
