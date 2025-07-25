ea59ade0b175aca0b901e043ef16a6027ac52139
MagouG <magueyegou@gmail.com>
2025-07-25 20:24:19 +0000

    Creation du dahsboard admin et architecture

---
+++ school-isi-frontend/src/app/features/admin/pages/tuteur/tuteur.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { TuteurRoutingModule } from './tuteur-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    TuteurRoutingModule
+  ]
+})
+export class TuteurModule { }
