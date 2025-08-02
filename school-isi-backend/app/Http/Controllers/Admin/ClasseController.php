da02ca054728370046c2e12f16e654e1eedd0a25
coumbafallydiokhane@gmail.com <maman@gamil.com>
2025-08-02 17:06:49 +0000

    Implémentation du CRUD pour les Classes
    Création du contrôleur, du modèle et des routes pour la gestion des classes.

--- school-isi-backend/app/Http/Controllers/Admin/ClasseController.php
+++ school-isi-backend/app/Http/Controllers/Admin/ClasseController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use App\Models\Classe;
 use Illuminate\Http\Request;
+
 use Illuminate\Support\Facades\Validator;
 
 class ClasseController extends Controller
